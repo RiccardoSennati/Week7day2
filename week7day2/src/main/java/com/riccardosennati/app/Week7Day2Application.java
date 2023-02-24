2d68347843b81ed404b2789b3ed6ea715354d17a
RiccardoSennati <rsennati@gmail.com>
2023-02-24 16:42:48 +0100

    ok

---
+++ week7day2/src/main/java/com/riccardosennati/app/Week7Day2Application.java
@@ -0,0 +1,13 @@
+package com.riccardosennati.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7Day2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week7Day2Application.class, args);
+	}
+
+}
---
+++ week7day2/src/test/java/com/riccardosennati/app/Week7Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riccardosennati.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7Day2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
